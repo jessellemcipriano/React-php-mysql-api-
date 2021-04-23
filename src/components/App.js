51ce349b1823d1b7f1cc451bfe3e07ee7122c453
Jéssellem S. Cipriano <69681532+jessellemcipriano@users.noreply.github.com>
2021-04-23 11:29:03 -0300

    feat: tela de login

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,74 @@
 import React from 'react';
-import Tabela from './tabela';
+
+import Tabela from './Tabela';
+
+import './App.css'
+
+import LogoEurekka from "./logo.png"
+import LogoNav from "./logo-nav.png"
+
+
 
 
 export default class App extends React.Component{
-
-
+    
     constructor(){
         super();
         this.state=({ 
-            db:[]
+            db:[],
+            frase:[]
+
         });
-        this.exibirCursos();
+        
         
     }
 
-    
-    exibirCursos(){
 
-        fetch("http://127.0.0.1:8000/api/resp", {
-            method: 'GET',
+    SalvarFrase=()=> {
+        const data = {
+            "test" : this.state.test
+          }
+          const requestInf = {
+              method: 'POST',
+              body: JSON.stringify(data),
+              headers: new Headers({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                Authorization : `Bearer ${this.state.db} `
+              }),
+          }; 
+
+        fetch("http://127.0.0.1:8000/api/test", requestInf)
+    }
+
+
+
+
+    
+    RecuperarFrase=()=> {
+        
+
+
+        const requestInfo = {
             headers: new Headers({
-              Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9sb2dpbiIsImlhdCI6MTYxOTA1NzY2MywiZXhwIjoxNjE5MDYxMjYzLCJuYmYiOjE2MTkwNTc2NjMsImp0aSI6IkN5czhGR1hSNmIyMHNHQWsiLCJzdWIiOjIsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.Db_dzp2Fv8Mz3Fh4qooLcY5qZXI_FAxpEv3BwS3OpLc`,
+              'Content-Type': 'application/json',
+              'Accept': 'application/json',
+              Authorization : `Bearer ${this.state.db} `
             }),
-          })
-        .then((response)=>response.json())
-        .then((responseJson)=> { 
-            this.setState({
+        }; 
+
+
+
+        fetch("http://127.0.0.1:8000/api/resp", requestInfo).then((resp)=>resp.json())
+        .then((respJson)=> { 
+            console.log(JSON.stringify(respJson));  
+
+            this.setState({ 
                 
-                db:responseJson
+                frase:respJson[0].test
             });
             
-            console.log(this.state.db);
-            
+            console.log(this.state.frase);   
             
         })
     }
@@ -39,17 +76,84 @@ export default class App extends React.Component{
 
 
 
+
+
+    login=()=> {
+        const data = {
+            "email" : this.state.email,
+            "password" : this.state.password,
+          }
+          const requestLogin = {
+              method: 'POST',
+              body: JSON.stringify(data),
+              headers: new Headers({
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+               
+              }),
+          }; 
+
+          fetch("http://127.0.0.1:8000/api/login", requestLogin).then((response)=>response.json())
+          .then((responseJson)=> { 
+              this.setState({ 
+                  
+                  db:responseJson.access_token
+              });
+              
+              console.log(this.state.db);
+              
+              
+              
+          })
+    }
+
+    setFrase=(value)=>{
+
+        this.setState({
+            test:value
+        })
+    }
+
+    setEmail=(value)=>{
+
+        this.setState({
+            email:value
+        })
+    }
+
+    setPassword=(value)=>{
+
+        this.setState({
+            password:value
+        })
+    }
+
     render(){
         return(
-            <div>
+            <div className="bg" >
+                <div className="nav-menu">
+                <img className= "img-nav" src={LogoNav }/>
+                </div>
+                <div className="container">
+                        <div className="blackCard container">  
+                        <img className= "imagem App-logo" src={LogoEurekka }/>
+                        <input className = "input in" placeholder = "E-mail" value = {this.state.email} onChange = {(evt) => this.setEmail(evt.target.value)}/><br></br>
+                        <input className = "input in" placeholder = "Senha" value = {this.state.password} onChange = {(evt) => this.setPassword(evt.target.value)}/><br></br>
+                        <button className = "btn" onClick = {this.login}>Login </button>
+                        <div>Token do login: {this.state.db}</div>
+                        </div>
+               </div> 
+                <br></br>
+
+               <div>
+               <input  className = "input" placeholder = "Frase" value = {this.state.test} onChange = {(evt) => this.setFrase(evt.target.value)}/><br></br>
+               <button onClick = {this.SalvarFrase}> Enviar frase de teste </button><br></br>
+               <button onClick = {this.RecuperarFrase}> Ler frase do bd </button>
+               <div>{this.state.frase}</div> 
                
-               < Tabela arrayCursos={this.state.db} />
-               
+               </div>
                  
-                 hello
             </div>
         );
     }
 }
-
-
